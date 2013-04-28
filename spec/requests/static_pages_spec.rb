cd7e8f5a1ecb84162d5329a2232f0e14acadd3cb
courington <chase.courington+github@gmail.com>
2013-04-28 10:02:34 -0600

    add contact us page

--- spec/requests/static_pages_spec.rb
+++ spec/requests/static_pages_spec.rb
@@ -3,7 +3,7 @@ require 'spec_helper'
 describe "StaticPages" do
 
   describe "Home Page" do
-    it "should have the content 'Sample App'" do
+    it "should have the header 'Sample App'" do
       visit '/static_pages/home'
       page.should have_selector('h1', :text => 'Sample App')
     end
@@ -14,7 +14,7 @@ describe "StaticPages" do
   end
 
   describe "Help Page" do
-    it "should have the content 'Help'" do
+    it "should have the header 'Help'" do
       visit '/static_pages/help'
       page.should have_selector('h1', :text => 'Help')
     end
@@ -25,13 +25,24 @@ describe "StaticPages" do
   end
 
   describe "About Page" do
-    it "should have the content 'About Us'" do
+    it "should have the header 'About Us'" do
       visit '/static_pages/about'
       page.should have_selector('h1', :text => 'About Us')
     end
     it "should have the right title" do
       visit '/static_pages/about'
       page.should have_selector('title', :text => 'Ruby on Rails Tutorial Sample App | About Us')
+    end
+  end
+
+  describe "Contact Page" do
+    it "should have the header 'Contact Us'" do
+      visit '/static_pages/contact'
+      page.should have_selector('h1', :text => 'Contact Us')
+    end
+    it "should have the right title" do
+      visit '/static_pages/contact'
+      page.should have_selector('title', :text => 'Ruby on Rails Tutorial Sample App | Contact Us')
     end
   end
 
