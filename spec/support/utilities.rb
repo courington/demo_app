daa73c310e1086118c8d98af64720f3859687365
courington <chase.courington+github@gmail.com>
2013-05-02 10:35:57 -0600

    adding new user actions and tests

---
+++ spec/support/utilities.rb
@@ -0,0 +1,9 @@
+# test for the full page title
+def full_title(page_title)
+  base_title = "Ruby on Rails Tutorial Sample App"
+  if page_title.empty?
+    base_title
+  else
+    "#{base_title} | #{page_title}"
+  end
+end
