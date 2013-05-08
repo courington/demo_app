30c8cb0fc9e371e9c255a888e96e720ce9810ce2
courington <chase.courington+github@gmail.com>
2013-05-08 16:29:50 -0600

    finish sign in

--- config/routes.rb
+++ config/routes.rb
@@ -5,10 +5,15 @@ DemoApp::Application.routes.draw do
   match '/help',    to: 'static_pages#help'
   match '/about',   to: 'static_pages#about'
   match '/contact', to: 'static_pages#contact'
+
+  # users
+  resources :users
   match '/signup',  to: 'users#new'
 
-  #users
-  resources :users
+  # sessions
+  resources :sessions, only: [:new, :create, :destroy]
+  match '/signin',  to: 'sessions#new'
+  match '/signout', to: 'sessions#destroy', via: :delete
 
   # The priority is based upon order of creation:
   # first created -> highest priority.
