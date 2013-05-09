86dea71e5f634dbd547b873cef8c60f49b5e19a1
courington <chase.courington+github@gmail.com>
2013-05-08 22:59:57 -0600

    pagination

--- spec/factories.rb
+++ spec/factories.rb
@@ -1,7 +1,7 @@
 FactoryGirl.define do
   factory :user do
-    name      "Chase Courington"
-    email     "chase@example.com"
+    sequence(:name) { |n| "Person #{n}" }
+    sequence(:email) { |n| "person_#{n}@example.com" }
     password  "foobar"
     password_confirmation "foobar"
   end
