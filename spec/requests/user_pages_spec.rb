e47ec1884bb8da35aa90fa612dd55eb461a47887
courington <chase.courington+github@gmail.com>
2013-05-08 19:09:42 -0600

    editing profile authentication

--- spec/requests/user_pages_spec.rb
+++ spec/requests/user_pages_spec.rb
@@ -60,7 +60,10 @@ describe "User pages" do
 
   describe "edit" do
     let(:user) { FactoryGirl.create(:user) }
-    before { visit edit_user_path(user) }
+    before do
+      sign_in user
+      visit edit_user_path(user)
+    end
 
     describe "page" do
       it { should have_selector('h1',    text: "Update your profile") }
@@ -75,14 +78,21 @@ describe "User pages" do
     end
 
     describe "with valid information" do
-      let(:user) { FactoryGirl.create(:user) }
-      before { sing_in user }
+      let(:new_name)  { "New Name" }
+      let(:new_email) { "new@example.com" }
+      before do
+        fill_in "Name",             with: new_name
+        fill_in "Email",            with: new_email
+        fill_in "Password",         with: user.password
+        fill_in "Confirm Password", with: user.password
+        click_button "Save changes"
+      end
 
-      it { should have_selector('title', text: user.name) }
-      it { should have_link('Profile',  href: user_path(user)) }
-      it { should have_link('Settings', href: edit_user_path(user)) }
+      it { should have_selector('title', text: new_name) }
+      it { should have_selector('div.alert.alert-success') }
       it { should have_link('Sign out', href: signout_path) }
-      it { should_not have_link('Sign in', href: signin_path) }
+      specify { user.reload.name.should  == new_name }
+      specify { user.reload.email.should == new_email }
     end
   end
 
